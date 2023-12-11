d5e38b0551574a02da551041a9f1750d62d86b40
Guillaume MEDA <98587375+GuillaumeMed@users.noreply.github.com>
2023-12-11 11:21:05 +0100

    update mod_regrid

--- src/mod_regrid.py
+++ src/mod_regrid.py
@@ -4,7 +4,7 @@ import pyinterp
 import pyinterp.fill
 import logging 
 
-def rec_regrid(ds_source, ds_target):
+def rec_regrid(ds_source, ds_target, field='ssh'):
     
     logging.info('     Regridding...')
     
@@ -12,8 +12,8 @@ def rec_regrid(ds_source, ds_target):
     x_source_axis = pyinterp.Axis(ds_source["lon"][:].values, is_circle=False)
     y_source_axis = pyinterp.Axis(ds_source["lat"][:].values)
     z_source_axis = pyinterp.TemporalAxis(ds_source["time"][:].values)
-    ssh_source = ds_source["ssh"][:].T
-    grid_source = pyinterp.Grid3D(x_source_axis, y_source_axis, z_source_axis, ssh_source.data)
+    field_source = ds_source[field][:].T
+    grid_source = pyinterp.Grid3D(x_source_axis, y_source_axis, z_source_axis, field_source.data)
     
     # Define target grid
     mx_target, my_target, mz_target = numpy.meshgrid(ds_target['lon'].values,
@@ -21,28 +21,28 @@ def rec_regrid(ds_source, ds_target):
                                                      z_source_axis.safe_cast(ds_target['time'].values),
                                                      indexing="ij")
     # Spatio-temporal Interpolation
-    ssh_interp = pyinterp.trivariate(grid_source,
+    field_interp = pyinterp.trivariate(grid_source,
                                      mx_target.flatten(),
                                      my_target.flatten(),
                                      mz_target.flatten(),
                                      bounds_error=False).reshape(mx_target.shape).T
     
     # MB add extrapolation in NaN values if needed
-    if numpy.isnan(ssh_interp).any():
-        logging.info('     NaN found in ssh_interp, starting extrapolation...')
+    if numpy.isnan(field_interp).any():
+        logging.info('     NaN found in field_interp, starting extrapolation...')
         x_source_axis = pyinterp.Axis(ds_target['lon'].values, is_circle=False)
         y_source_axis = pyinterp.Axis(ds_target['lat'].values)
         z_source_axis = pyinterp.TemporalAxis(ds_target["time"][:].values)
-        grid = pyinterp.Grid3D(x_source_axis, y_source_axis, z_source_axis,  ssh_interp.T)
+        grid = pyinterp.Grid3D(x_source_axis, y_source_axis, z_source_axis,  field_interp.T)
         has_converged, filled = pyinterp.fill.gauss_seidel(grid)
     else:
-        filled = ssh_interp.T
+        filled = field_interp.T
     
     # Save to dataset
-    ds_ssh_interp = xr.Dataset({'ssh' : (('time', 'lat', 'lon'), filled.T)},
+    ds_field_interp = xr.Dataset({field : (('time', 'lat', 'lon'), filled.T)},
                                coords={'time': ds_target['time'].values,
                                        'lon': ds_target['lon'].values, 
                                        'lat': ds_target['lat'].values, 
                                        })
     
-    return ds_ssh_interp
+    return ds_field_interp
